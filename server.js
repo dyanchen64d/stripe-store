d7fd398357332856fecb03a719d57469e528b31e
chen.zhang <zhangchen64d@gmail.com>
2023-02-20 19:40:08 +0900

    update

---
+++ server.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+const app = express();
+app.use(cors());
+app.use(express.static('public'));
+app.use(express.json());
+
+app.post('/checkout', async (req, res) => {
+  //
+});
